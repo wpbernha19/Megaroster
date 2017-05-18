54bd6fd42312241ee9a18231a50d6e4181bfbf7e
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-18 03:36:51 -0400

    Add students to array and reset the form

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
 $(document).foundation()
 
 const megaroster = {
-    init() {
-        this.max = 0
-        document
-            .querySelector('#new-student')
-            .addEventListener('submit', this.addStudent.bind(this))
-    },
+  students: [],
 
-    addStudent(ev) {
-        event.preventDefault()
-        const f = ev.target
-        const student = {
-            id: this.max + 1,
-            name: f.studentName.value,
-        }
+  init() {
+    this.studentList = document.querySelector('#student-list')
+    this.max = 0
+    document
+      .querySelector('#new-student')
+      .addEventListener('submit', this.addStudent.bind(this))
+  },
 
-        this.buildListItem(student)
-        this.max ++
-    },
+  addStudent(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const student = {
+      id: this.max + 1,
+      name: f.studentName.value,
+    }
 
-    buildListItem(student) {
-        console.log(student)
-    },
+    this.students.push(student)
+    
+    const listItem = this.buildListItem(student)
+    this.studentList.appendChild(listItem)
+    
+    this.max ++
+    f.reset()
+  },
 
+  buildListItem(student) {
+    const li = document.createElement('li')
+    li.textContent = student.name
+    return li
+  },
 }
 megaroster.init()
