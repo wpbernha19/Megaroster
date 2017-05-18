a33aa883a441cf62fa571bb64423d98db5d0d5ea
Billy Bernhardt <wpbernha19@wabash.edu>
2017-05-18 02:55:28 -0400

    Put index.html in the right directory

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation()
+
+const megaroster = {
+    init() {
+        document
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudent)
+    },
+
+    addStudent(ev) {
+        event.preventDefault()
+        const studentName = ev.target.studentName.value
+        console.log(studentName)
+    },
+
+}
+megaroster.init()
